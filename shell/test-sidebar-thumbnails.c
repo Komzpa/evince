a048fef11c1705d9d3c095b57c5d749d7f45646c
Darafei Praliaskouski <me@komzpa.net>
2026-05-13 13:17:14 +0400

    fix(sidebar): render initial thumbnail textures reliably

--- shell/test-sidebar-thumbnails.c
+++ shell/test-sidebar-thumbnails.c
@@ -11,6 +11,34 @@
 
 #include "ev-sidebar-thumbnails-private.h"
 
+static guint
+count_nonwhite_texture_pixels (GdkTexture *texture)
+{
+	gsize stride;
+	guchar *data;
+	guint count = 0;
+
+	stride = gdk_texture_get_width (texture) * 4;
+	data = g_malloc0 (stride * gdk_texture_get_height (texture));
+	gdk_texture_download (texture, data, stride);
+
+	for (gint y = 0; y < gdk_texture_get_height (texture); y++) {
+		guchar *row = data + y * stride;
+
+		for (gint x = 0; x < gdk_texture_get_width (texture); x++) {
+			guchar *pixel = row + x * 4;
+
+			if (pixel[0] < 245 || pixel[1] < 245 || pixel[2] < 245) {
+				count++;
+			}
+		}
+	}
+
+	g_free (data);
+
+	return count;
+}
+
 static void
 test_thumbnail_target_size_uses_logical_pixels (void)
 {
@@ -44,6 +72,36 @@ test_thumbnail_target_size_clamps_extreme_aspect_ratio (void)
 	g_assert_cmpint (height, ==, 1);
 }
 
+static void
+test_thumbnail_texture_keeps_page_pixels_after_surface_destroy (void)
+{
+	cairo_surface_t *surface;
+	cairo_t *cr;
+	GdkTexture *texture;
+	guint nonwhite_pixels;
+
+	surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 160, 120);
+	cr = cairo_create (surface);
+
+	cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
+	cairo_paint (cr);
+	cairo_set_source_rgb (cr, 0.0, 0.2, 0.7);
+	cairo_rectangle (cr, 0, 0, 160, 120);
+	cairo_fill (cr);
+	cairo_destroy (cr);
+
+	texture = ev_sidebar_thumbnails_texture_new_for_surface (surface);
+	cairo_surface_destroy (surface);
+
+	g_assert_cmpint (gdk_texture_get_width (texture), ==, 160);
+	g_assert_cmpint (gdk_texture_get_height (texture), ==, 120);
+
+	nonwhite_pixels = count_nonwhite_texture_pixels (texture);
+	g_assert_cmpuint (nonwhite_pixels, >, 160 * 120 / 2);
+
+	g_object_unref (texture);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -55,6 +113,8 @@ main (int argc, char **argv)
 			 test_thumbnail_target_size_rotates_logical_pixels);
 	g_test_add_func ("/sidebar-thumbnails/target-size/clamps-extreme-aspect-ratio",
 			 test_thumbnail_target_size_clamps_extreme_aspect_ratio);
+	g_test_add_func ("/sidebar-thumbnails/texture/keeps-page-pixels-after-surface-destroy",
+			 test_thumbnail_texture_keeps_page_pixels_after_surface_destroy);
 
 	return g_test_run ();
 }
